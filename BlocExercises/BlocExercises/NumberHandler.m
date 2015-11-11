288964989d902b6effc6036909ecb3ffb68387f9
Mark DuVall <mark@markduvall.com>
2015-11-10 19:03:53 -0800

    Assignment 15 complete

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,35 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSNumber *temp;
+    temp = @([number integerValue] * 2);
+    return temp;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *mutableArray = [[NSMutableArray alloc] init];
+    
+    for (NSInteger i=number; i<=otherNumber; i++){
+        NSNumber *num = [NSNumber numberWithInt:i];
+        [mutableArray addObject:num];
+    }
+    NSArray *array = [[NSArray alloc] initWithArray:mutableArray];
+
+    return array;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSInteger lowest = [[arrayOfNumbers firstObject] integerValue];
+    
+    for( NSNumber *i in arrayOfNumbers) {
+        if ([i intValue]< lowest) {
+            lowest = [i integerValue];
+        }
+    }
+    
+    return lowest;
 }
 
 @end
