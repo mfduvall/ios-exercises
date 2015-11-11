cced33f59b661d9df741d13e8f0e5949aa66ca65
Mark DuVall <mark@markduvall.com>
2015-11-10 18:59:13 -0800

    Assignment 13 completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,21 +12,34 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    NSArray *array = [characterString componentsSeparatedByString:@";"];
+    return array;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    NSString *string = [NSString stringWithFormat:@"%@;%@;%@", characterArray[0], characterArray[1], characterArray[2]];
+    return string;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *array = [[NSMutableArray alloc] initWithArray:characterArray];
+    NSSortDescriptor *sd = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [array sortUsingDescriptors: @[sd]];
+     
+    return array;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    BOOL bWorf = NO;
+    
+    for(NSString *string in characterArray) {
+        if ([string rangeOfString:@"worf" options:NSCaseInsensitiveSearch].location != NSNotFound) {
+            return YES;
+        }
+    }
     return NO;
 }
 
