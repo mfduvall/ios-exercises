3aebc238b503aeb85bcf489a464826b72da62259
Mark DuVall <mark@markduvall.com>
2015-11-10 19:01:46 -0800

    Assignment 14 complete

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,28 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    
+    return characterDictionary[@"favorite drink"];
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    for(NSDictionary *obj in charactersArray) {
+        array = [array arrayByAddingObject:obj[@"favorite drink"]];
+        
+    }
+    NSArray *returnArray = [[NSArray alloc] initWithArray:array];
+    return returnArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithDictionary:characterDictionary];
+    [dict setObject:@"Make it so" forKey:@"quote"];
+    NSDictionary *returnDictionary = [[NSDictionary alloc] initWithDictionary:dict];
+    return returnDictionary;
 }
 
 @end
